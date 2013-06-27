13f50654dbf4b68bf5235e9fe40b5e4792a07bd2
hblanco <hblanco@nsl-ubuntu-hblanco.(none)>
2013-06-27 12:13:39 -0700

    Added resolution of routes based on the files in folder routes

--- index.js
+++ index.js
@@ -1,34 +1,53 @@
 /************** TODO *******************
-*   1. Create a config file and its loader
+*1. Create a config file and its loader
 ****************************************/
 
 var fs = require('fs'),
     q  = require('q'),
     __ = require('underscore'),
+    path =  require('path'),
     findit = require('findit'),
     restify = require('restify'),
     server = restify.createServer(),
     config = require('./favoritous.config.json'),
+    httpVerbs = ['GET', 'POST', 'PUT', 'DELETE', 'HEAD'],
     load_setup,
     setup_routes,
     load_static_route,
     start_server;
 
 load_setup = function(){
+    console.log('load_setup');
+
     var deferred = q.defer(),
         finder = null,
-        route_regex = /^.\/\w+\/(\w+)\/((\w*).js)$/g;
+        routeRegx = /\.\/routes\/(\w+).js/;
 
-    finder = findit.find(config.routes_folder);
+    finder = findit.find(config.routesFolder);
 
     finder.on('file', function(file, stat){
-        var matches = route_regex.exec(file);
+        console.log('Findit Finder On File: ' + file);
 
-        console.log('Match 1: ' + matches[1]);
-        console.log('Match 2: ' + matches[2]);
+        var match   = routeRegx.exec(file),
+            verb    = match[1],
+            VERB    = verb.toUpperCase(),
+            actions = require('./' + path.join(file))[VERB];
+
+        if(verb === 'delete') { verb = 'del'; }
+
+        //This the file name does match a verb, skip it
+        if(!__.contains(httpVerbs, VERB)){ return; }
+
+        for(route in actions){
+            if(actions.hasOwnProperty(route)){
+                //TODO: Refactor building route string
+                server[verb](config.apiUrl + '/' + route, actions[route])
+            }
+        }
     });
 
     finder.on('end', function(){
+        console.log('end');
         deferred.resolve();
     });
 
@@ -36,17 +55,20 @@ load_setup = function(){
 };
 
 load_static_route = function(){
+    console.log('load_static_route');
+
     server.get(/\/*.*/, restify.serveStatic({
-        directory: config.static_folder
+        directory: config.contentFolder
     }));
 
     server.get( /\/images\/*.*/, restify.serveStatic(
     {
-        directory: config.static_folder + '/' + 'images'
+        directory: config.contentFolder + '/' + 'images'
     }));
 };
 
 start_server = function(){
+    console.log("start_server");
     server.listen(config.port, function(){
          console.log('%s listening at %s', server.name, server.url);
     });
@@ -54,7 +76,7 @@ start_server = function(){
 
 /******** Starting the Server **********************/
 
-if(!fs.existsSync(config.routes_folder)){
+if(!fs.existsSync(config.routesFolder)){
     throw new Error('Routes folder does not exists!');
 }
 
--- routes/get/version.js
+++
@@ -1,5 +0,0 @@
-version = function(req, res, next){
-	res.send({ version: '0.0.2-dev'});
-	next();
-}
-
