dc4bbbc6a957c916b5b5141339a7c7d3973ed19d
thanniaB <tblanchet@nearsoft.com>
2013-07-17 15:34:48 -0700

    window resize

--- scripts/sameHeight.js
+++ scripts/sameHeight.js
@@ -4,20 +4,26 @@
         setup = null;
 
     setup = function setup(){
-        divday.each(function(){
+        divday.each(columnSize);  
+        $(window).resize(columnSize);
+
+        function columnSize(){
             var divlinks = $(this).find('div.links'),
                 divimages = $(this).find('div.images'),                
                 linksHeight = divlinks.height(),
                 imagesHeight = divimages.height();
 
-        	if(imagesHeight >= linksHeight){
+            if(imagesHeight >= linksHeight){
              divlinks.height(imagesHeight);
-         	} else {
-         	  divimages.height(linksHeight);
-         	}
-        });
+            } else {
+              divimages.height(linksHeight);
+            }
+        }      
     }
 
     $(window).load(setup);
+    
+
+
 
 }(jQuery))
