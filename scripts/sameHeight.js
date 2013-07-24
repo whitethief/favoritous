949202c73d4ec3e17e1a4c9578fccf638914839e
thanniaB <tblanchet@nearsoft.com>
2013-07-23 17:05:52 -0700

    ya cambia de lugar la barra de search :)

--- scripts/sameHeight.js
+++ scripts/sameHeight.js
@@ -17,15 +17,17 @@
 
         if (windowWidth < 850 && windowWidth > 450) {
             $("li#recent").html('<i class="icon-time"> </i> Recent'); 
- 
+            $("div.search").insertAfter("nav#menu"); 
         }
         else if (windowWidth <= 450){
             $("li#recent").html('<i class="icon-time"> </i>');
             $("li#archive").html('<i class="icon-folder-close"> </i>');
+            $("div.search").insertAfter("nav#menu"); 
         }  else {
                         
             $("li#recent").html('<i class="icon-time"> </i> Recent (last 24 hours)');
             $("li#archive").html('<i class="icon-folder-close"> </i> Archive');
+            $("nav#menu").insertAfter("div.search"); 
             
 
         }
